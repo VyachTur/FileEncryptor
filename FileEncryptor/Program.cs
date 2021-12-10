e9502762ca06c4ab4102984401674901965b1826
tampl <tampl@CASTLE>
2021-12-11 03:35:55 +1000

    Рефакторинг

---
+++ FileEncryptor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileEncryptor.WPF
+{
+    internal static class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+
+
+    }
+}
